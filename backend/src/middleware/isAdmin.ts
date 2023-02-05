49fa83f118533a002e8ef948bb227e2ac8e30cb2
abdulsagheer <abdulsagheeras29@gmail.com>
2023-02-05 19:18:08 +0530

    Added Header for Admin token access

---
+++ backend/src/middleware/isAdmin.ts
@@ -0,0 +1,34 @@
+// Importing Libraries
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import expressAsyncHandler from 'express-async-handler';
+
+// Importing Dependencies
+import User from '../model/User.Model';
+import Api, { Message } from '../utils/helper';
+import { JwtPayload } from '../interfaces/Jwt';
+
+export const isAdmin = expressAsyncHandler(
+	async (req: any, res: any, next: NextFunction) => {
+		let token: any;
+		try {
+			token = req.headers.authorization?.split(' ')[1];
+			if (!token) {
+				return res.status(401).send('Access denied. No token provided.');
+			}
+			const decoded = jwt.verify(
+				token,
+				String(process.env.JWT_KEY)
+			) as JwtPayload;
+			// find the user by ID
+			const user = await User.findById(decoded?.id).select('-password');
+			req.user = decoded as JwtPayload;
+			if (req.user.role !== 'admin') {
+				return Api.unauthorized(req, res, Message.NotAuthorized);
+			}
+			next();
+		} catch (error: any) {
+			Api.serverError(req, res, error, Message.ServerError);
+		}
+	}
+);
