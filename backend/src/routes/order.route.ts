9ad9f8d309ffcf7d9ffdb4a8adb42875fbdf1dc5
abdulsagheer <abdulsagheeras29@gmail.com>
2023-02-06 14:58:22 +0530

    Configured routes for stocks and orders

---
+++ backend/src/routes/order.route.ts
@@ -0,0 +1,28 @@
+// Importing Libraries
+import express from 'express';
+
+// Importing dependencies
+import {
+	createOrder,
+	executeSellOrders,
+} from '../controllers/order.controller';
+import { authMiddleware } from '../middleware/auth';
+
+const stockRoute = express.Router();
+
+/** Get create stocks */
+stockRoute.post('/buy', authMiddleware, createOrder);
+
+/** Fetch stock by id */
+stockRoute.put('/sell', authMiddleware, executeSellOrders);
+
+/** Delete all stocks */
+stockRoute.delete('/:id', authMiddleware, deleteStock);
+
+/** Fetch all stocks */
+stockRoute.get('/', authMiddleware, getStocks);
+
+/** Fetch Single User Details By ID */
+stockRoute.get('/:id', authMiddleware, getStockById);
+
+export default stockRoute;
--- backend/src/routes/stock.route.ts
+++ backend/src/routes/stock.route.ts
@@ -12,21 +12,21 @@ import {
 import { authMiddleware } from '../middleware/auth';
 import { isAdmin } from '../middleware/isAdmin';
 
-const stockRoute = express.Router();
+const orderRoute = express.Router();
 
 /** Get create stocks */
-stockRoute.post('/create', authMiddleware, isAdmin, createStock);
+orderRoute.post('/create', authMiddleware, isAdmin, createStock);
 
 /** Fetch stock by id */
-stockRoute.put('/:id', authMiddleware, isAdmin, updateStock);
+orderRoute.put('/:id', authMiddleware, isAdmin, updateStock);
 
 /** Delete all stocks */
-stockRoute.delete('/:id', authMiddleware, isAdmin, deleteStock);
+orderRoute.delete('/:id', authMiddleware, isAdmin, deleteStock);
 
 /** Fetch all stocks */
-stockRoute.get('/', authMiddleware, isAdmin, getStocks);
+orderRoute.get('/', authMiddleware, isAdmin, getStocks);
 
 /** Fetch Single User Details By ID */
-stockRoute.get('/:id', authMiddleware, isAdmin, getStockById);
+orderRoute.get('/:id', authMiddleware, isAdmin, getStockById);
 
-export default stockRoute;
+export default orderRoute;
